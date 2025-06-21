9b7f4de151d2102e68a618cd6d58983cb5185330
ZhirDev <zheerjalal22@gmail.com>
2025-06-22 02:33:30 +0300

    pedany Scripts.js

---
+++ Scripts.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
