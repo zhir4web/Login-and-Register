aa69881fce1e4f9f7d9e4a0db7d19ec944ec896c
ZhirDev <zheerjalal22@gmail.com>
2025-06-22 03:43:59 +0300

    update gshtim krd

--- Scripts.js
+++ Scripts.js
@@ -1,7 +1,7 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
+const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
